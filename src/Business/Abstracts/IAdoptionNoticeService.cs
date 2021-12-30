c28f09cce6aa9d762002fc4bb51f75790e8934a9
Berat Yeşbek <beratyesbek@gmail.com>
2021-12-30 14:03:55 +0300

    file directory changed

---
+++ src/Business/Abstracts/IAdoptionNoticeService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Result.Abstracts;
+using Entity.concretes;
+
+namespace Business.Abstracts
+{
+    public interface IAdoptionNoticeService
+    {
+        IDataResult<AdoptionNotice> Add(AdoptionNotice adoptionNotice);
+
+        IResult Update(AdoptionNotice adoptionNotice);
+
+        IResult Delete(AdoptionNotice adoptionNotice);
+
+        IDataResult<AdoptionNotice> Get(int id);
+
+        IDataResult<List<AdoptionNotice>> GetAll();
+    }
+}
---
+++ src/DataAccess/ConnectionString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public static class ConnectionString
+    {
+        public static string DataBaseConnectionString { get; set; } =
+            "Server=postgres;Database=nerviodb;Port=5432;Username=postgres;Password=123456";
+    }
+}
