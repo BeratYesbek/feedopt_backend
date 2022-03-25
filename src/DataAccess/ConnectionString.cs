56edf8721c06723ae0ed8a22af99c988cda208aa
Berat Yeşbek <beratyesbek@hotmail.com>
2022-03-25 21:04:40 +0300

    gitignore && db credentials fix

--- src/DataAccess/ConnectionString.cs
+++ src/DataAccess/ConnectionString.cs
@@ -8,7 +8,8 @@ namespace DataAccess
 {
     public static class ConnectionString
     {
+        
         public static string DataBaseConnectionString { get; set; } =
-            "User ID=fywlupxssyiqqs;Password=5b93684569ecf1e95565dae643ca6d5b715c62bc94b4b72b1b292a7501377917;Host=ec2-35-175-68-90.compute-1.amazonaws.com;Port=5432;Database=d2ll511g04mrkq;Pooling=true;SSL Mode=Require;TrustServerCertificate=True;";
+            "User ID=qjqxswqywlfufk;Password=bb5b8cc0ff3a8d6bbf7842987df216d23667ca09755fb687d052be67cf3b90e6;Host=ec2-52-3-60-53.compute-1.amazonaws.com;Port=5432;Database=d2emgi1idit262;Pooling=true;SSL Mode=Require;TrustServerCertificate=True;";
     }
 }
