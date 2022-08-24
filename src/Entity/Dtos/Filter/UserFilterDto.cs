ef5d65b995104fee68c2847e261929abe613d03a
Feanor <beratyesbek@gmail.com>
2022-08-24 11:58:26 +0300

    User filter dto fix

--- src/Entity/Dtos/Filter/UserFilterDto.cs
+++ src/Entity/Dtos/Filter/UserFilterDto.cs
@@ -3,8 +3,8 @@ namespace Entity.Dtos.Filter;
 public class UserFilterDto
 {
     public int[] OperationClaimId { get; set; }
-    
-    public bool Status { get; set; }    
-    
-    public bool EmailConfirmed { get; set; }
+
+    public bool? Status { get; set; } = null;
+
+    public bool? EmailConfirmed { get; set; } = null;
 }
